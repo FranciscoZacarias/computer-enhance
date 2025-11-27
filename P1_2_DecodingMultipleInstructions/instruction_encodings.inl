4e4c87cf151a478d734344f7e26a058313a87998
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2025-11-27 16:42:30 +0100

    re-write to handle - hopefully - most of encodings

--- P1_2_DecodingMultipleInstructions/instruction_encodings.inl
+++ P1_2_DecodingMultipleInstructions/instruction_encodings.inl
@@ -5,18 +5,18 @@
 
 // Register/memory to/from register 
 {
-  .name=Sl("mov"),
-  .data_transfer_type=DataTransfer_RegisterMemory_ToFrom_Register,
-  .instruction=0b100010'00,
-  .opcode_mask=0b111111'00,
-  .data_size=2,
-  .data=0,
-  .S=bit_field_non_existant(),
-  .W=bit_field(6,0b1),
-  .D=bit_field(7,0b1),
-  .V=bit_field_non_existant(),
-  .Z=bit_field_non_existant(),
-  .MOD=bit_field(8, 0b11),
-  .REG=bit_field(10,0b111),
-  .R_M=bit_field(13,0b111)
+  .name = Sl("mov"),
+  .data_transfer_type = DataTransfer_RegisterMemory_ToFrom_Register,
+  .instruction        = 0b100010'00,
+  .opcode_mask        = 0b111111'00,
+  .data_size          = 2,
+  .data               = 0,
+  .S   = bit_field_non_existant(),
+  .W   = bit_field(8,0b1),
+  .D   = bit_field(9,0b1),
+  .V   = bit_field_non_existant(),
+  .Z   = bit_field_non_existant(),
+  .MOD = bit_field(6,0b11),
+  .REG = bit_field(3,0b111),
+  .R_M = bit_field(0,0b111)
 },
--- P1_2_DecodingMultipleInstructions/p1_2.c
+++ P1_2_DecodingMultipleInstructions/p1_2.c
@@ -18,21 +18,21 @@ enum
 typedef u8 Mod_Type;
 enum
 {
-  Mod_MemoryMode_NoDisplacement, // NOTE(fz): Except when R/M is 110, then 16bit displacement
-  Mod_MemoryMode_8BitDisplacement,
-  Mod_MemoryMode_16BitDisplacement,
-  Mod_RegisterMode_NoDisplacement,
+  Mod_MemoryMode_NoDisplacement    = 0b00, // NOTE(fz): Except when R/M is 110, then 16bit displacement
+  Mod_MemoryMode_8BitDisplacement  = 0b01,
+  Mod_MemoryMode_16BitDisplacement = 0b10,
+  Mod_RegisterMode_NoDisplacement  = 0b11,
 };
 
 typedef struct Bit_Field Bit_Field;
 struct Bit_Field
 {
-  u8 start_offset; // Offset from the left! If field doesnt exist then INVALID_OFFSET. 
+  u8 offset; // If field doesnt exist then INVALID_OFFSET. 
   u8 mask;
   u8 data;
 };
-#define bit_field(o,m) {.start_offset=(o),.mask=(m), .data=0}
-#define bit_field_non_existant() {.start_offset=INVALID_OFFSET,.mask=0, .data=0}
+#define bit_field(o,m) {.offset=(o),.mask=(m), .data=0}
+#define bit_field_non_existant() {.offset=INVALID_OFFSET,.mask=0, .data=0}
 
 typedef struct Instruction_Encoding Instruction_Encoding;
 struct Instruction_Encoding
@@ -48,8 +48,8 @@ struct Instruction_Encoding
     u16 data; // Data payload when parsing the binary
     struct
     {
-      u8 data_low;
       u8 data_high;
+      u8 data_low;
     };
   };
 
@@ -72,6 +72,12 @@ global Instruction_Encoding instructions[] = {
 };
 
 #define LISTING_FILE "listing_0039_more_movs"
+
+function inline u8
+get_bitfields(u16 data, u8 offset, u8 mask)
+{
+  return (data >> offset) & mask;
+}
 
 /*
   There are quite a few string related leaks (leaked functions are marked in francisco.h)
@@ -110,73 +116,101 @@ main()
 
         // Fill data
         instruction.data_low = low_byte;
-        if (instruction.data_size == 2)
-        {
-          instruction.data_high = compiled_original_listing.cstring[byte_count++];
-        }
+        if (instruction.data_size == 2) instruction.data_high = compiled_original_listing.cstring[byte_count++];
+
+        if (instruction.S.offset != INVALID_OFFSET) instruction.S.data = get_bitfields(instruction.data, instruction.S.offset, instruction.S.mask);
+        if (instruction.W.offset != INVALID_OFFSET) instruction.W.data = get_bitfields(instruction.data, instruction.W.offset, instruction.W.mask);
+        if (instruction.D.offset != INVALID_OFFSET) instruction.D.data = get_bitfields(instruction.data, instruction.D.offset, instruction.D.mask);
+        if (instruction.V.offset != INVALID_OFFSET) instruction.V.data = get_bitfields(instruction.data, instruction.V.offset, instruction.V.mask);
+        if (instruction.Z.offset != INVALID_OFFSET) instruction.Z.data = get_bitfields(instruction.data, instruction.Z.offset, instruction.Z.mask);
+
+        if (instruction.MOD.offset != INVALID_OFFSET) instruction.MOD.data = get_bitfields(instruction.data, instruction.MOD.offset, instruction.MOD.mask);
+        if (instruction.REG.offset != INVALID_OFFSET) instruction.REG.data = get_bitfields(instruction.data, instruction.REG.offset, instruction.REG.mask);
+        if (instruction.R_M.offset != INVALID_OFFSET) instruction.R_M.data = get_bitfields(instruction.data, instruction.R_M.offset, instruction.R_M.mask);
 
         break;
       }
     }
-  }
 
-  printf("\n```%s\n```", output_buffer);
-
-#if 0
-    for (u32 i = 0; i < array_count(ops); i += 1)
+    switch (instruction.data_transfer_type)
     {
-      Op op = ops[i];
-      u8 opcode = first_byte;
-
-      // Register/Memory to/from Register
-      opcode >>= 2;
-      if (opcode == op.opcode)
+      case DataTransfer_RegisterMemory_ToFrom_Register:
       {
-        u8 D = first_byte & 0b10; // 1: REG is destination, 0: REG is NOT destination
-        u8 W = first_byte & 0b1;  // 1: Is 16 bits, 0: Is 8 bits
-      
-        String* table = (W ? reg_table_wide : reg_table);
-        String instruction = S("mov");
+        String* table = (instruction.W.data ? reg_table_wide : reg_table);
 
-        u8 second_byte = compiled_original_listing   .cstring[byte_count++];
-        u8 MOD = (second_byte >> 6) & 0b00000011;
-        u8 REG = (second_byte >> 3) & 0b00000111;
-        u8 R_M = (second_byte >> 0) & 0b00000111;
-
-        switch (MOD)
+        switch (instruction.MOD.data)
         {
-          case MOD_MemoryMode_NoDisplacement:
+          case Mod_MemoryMode_NoDisplacement:
           {
-            if (R_M == 0b110)
-            {
-              printf("Memory mode no displacement with R/M=110 (I.e. but actually with 16bit displacement) not implemented.");
-              return;
-            }
-            
-            printf("MOD_MemoryMode_NoDisplacement not implemented.\n");
+            printf("Mod_MemoryMode_NoDisplacement not implemented.\n");
+            goto end;
           }
           break;
-          case MOD_MemoryMode_8BitDisplacement:
+          case Mod_MemoryMode_8BitDisplacement:
           {
-            printf("MOD_MemoryMode_8BitDisplacement not implemented.\n");
+            printf("Mod_MemoryMode_8BitDisplacement not implemented.\n");
+            goto end;
           }
           break;
-          case MOD_MemoryMode_16BitDisplacement:
+          case Mod_MemoryMode_16BitDisplacement:
           {
-            printf("MOD_MemoryMode_16BitDisplacement not implemented.\n");
+            printf("Mod_MemoryMode_16BitDisplacement not implemented.\n");
+            goto end;
           }
           break;
-          case MOD_RegisterMode_NoDisplacement:
+          case Mod_RegisterMode_NoDisplacement:
           {
-            String destination =  D ? table[R_M] : table[REG];
-            String source      = !D ? table[R_M] : table[REG];
-
-            sprintf(output_buffer, "%s\n%s %s, %s", output_buffer, instruction.cstring, source.cstring, destination.cstring);
+            String destination =  instruction.D.data ? table[instruction.R_M.data] : table[instruction.REG.data];
+            String source      = !instruction.D.data ? table[instruction.R_M.data] : table[instruction.REG.data];
+            sprintf(output_buffer, "%s\nmov %s, %s", output_buffer, source.cstring, destination.cstring);
           }
           break;
         }
       }
+      break;
+      case DataTransfer_Immediate_To_RegisterMemory:
+      {
+        printf("DataTransfer_Immediate_To_RegisterMemory not implemented.\n");
+        goto end;
+      }
+      break;
+      case DataTransfer_Immediate_To_Register:
+      {
+        printf("DataTransfer_Immediate_To_Register not implemented.\n");
+        goto end;
+      }
+      break;
+      case DataTransfer_Memory_To_Accumulator:
+      {
+        printf("DataTransfer_Memory_To_Accumulator not implemented.\n");
+        goto end;
+      }
+      break;
+      case DataTransfer_Accumulator_To_Memory:
+      {
+        printf("DataTransfer_Accumulator_To_Memory not implemented.\n");
+        goto end;
+      }
+      break;
+      case DataTransfer_RegisterMemory_To_SegmentRegister:
+      {
+        printf("DataTransfer_RegisterMemory_To_SegmentRegister not implemented.\n");
+        goto end;
+      }
+      break;
+      case DataTransfer_SegmentRegister_To_RegisterMemory:
+      {
+        printf("DataTransfer_SegmentRegister_To_RegisterMemory not implemented.\n");
+        goto end;
+      }
+      break;
     }
+  }
+
+end:
+  printf("\n```%s\n```", output_buffer);
+
+#if 0
 
   write_file(decompiled_asm_path, output_buffer, strlen(output_buffer));
 
