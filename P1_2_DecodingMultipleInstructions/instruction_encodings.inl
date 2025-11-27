0ece9bfd8e26174a5c1d81a7c1024f501ece8f92
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2025-11-27 18:26:03 +0100

    listing 39 done

--- P1_2_DecodingMultipleInstructions/instruction_encodings.inl
+++ P1_2_DecodingMultipleInstructions/instruction_encodings.inl
@@ -2,15 +2,14 @@
 
 // MOV
 // ---
-
-// Register/memory to/from register 
 {
+  // Register/memory to/from register 
   .name = Sl("mov"),
   .data_transfer_type = DataTransfer_RegisterMemory_ToFrom_Register,
   .instruction        = 0b100010'00,
   .opcode_mask        = 0b111111'00,
-  .data_size          = 2,
-  .data               = 0,
+  .encoding_size      = 2,
+  .encoding           = 0,
   .S   = bit_field_non_existant(),
   .W   = bit_field(8,0b1),
   .D   = bit_field(9,0b1),
@@ -19,4 +18,40 @@
   .MOD = bit_field(6,0b11),
   .REG = bit_field(3,0b111),
   .R_M = bit_field(0,0b111)
+},
+
+{
+  // Immediate to register/memory
+  .name = Sl("mov"),
+  .data_transfer_type = DataTransfer_Immediate_To_RegisterMemory,
+  .instruction        = 0b1100011'0,
+  .opcode_mask        = 0b1111111'0,
+  .encoding_size      = 2,
+  .encoding           = 0,
+  .S   = bit_field_non_existant(),
+  .W   = bit_field(8,0b1),
+  .D   = bit_field_non_existant(),
+  .V   = bit_field_non_existant(),
+  .Z   = bit_field_non_existant(),
+  .MOD = bit_field(6,0b11),
+  .REG = bit_field_non_existant(),
+  .R_M = bit_field(0,0b111)
+},
+
+{
+  // Immediate to register
+  .name = Sl("mov"),
+  .data_transfer_type = DataTransfer_Immediate_To_Register,
+  .instruction        = 0b1011'0000,
+  .opcode_mask        = 0b1111'0000,
+  .encoding_size      = 1,
+  .encoding           = 0,
+  .S   = bit_field_non_existant(),
+  .W   = bit_field(11,0b1),
+  .D   = bit_field_non_existant(),
+  .V   = bit_field_non_existant(),
+  .Z   = bit_field_non_existant(),
+  .MOD = bit_field_non_existant(),
+  .REG = bit_field(8,0b111),
+  .R_M = bit_field_non_existant()
 },
--- P1_2_DecodingMultipleInstructions/p1_2.c
+++ P1_2_DecodingMultipleInstructions/p1_2.c
@@ -42,14 +42,14 @@ struct Instruction_Encoding
 
   u8 instruction; // Bit pattern of the instruction
   u8 opcode_mask; // I think the instruction encoding itself will always be on the first byte? So mask on the low byte should be enough
-  u8 data_size; // How many bytes this Instruction necessairly has (could still have more depending on the encoding).
+  u8 encoding_size; // How many bytes this Instruction necessairly has (could still have more depending on the encoding).
   union
   {
-    u16 data; // Data payload when parsing the binary
+    u16 encoding; // Data payload when parsing the binary
     struct
     {
-      u8 data_high;
-      u8 data_low;
+      u8 encoding_high;
+      u8 encoding_low;
     };
   };
 
@@ -64,8 +64,47 @@ struct Instruction_Encoding
   Bit_Field R_M;
 };
 
-global String reg_table[]      = { Sl("al"), Sl("cl"), Sl("dl"), Sl("bl"), Sl("ah"), Sl("ch"), Sl("dh"), Sl("bh") };
-global String reg_table_wide[] = { Sl("ax"), Sl("cx"), Sl("dx"), Sl("bx"), Sl("sp"), Sl("bp"), Sl("si"), Sl("di") };
+global String reg_table[] = {
+  Sl("al"),
+  Sl("cl"),
+  Sl("dl"),
+  Sl("bl"),
+  Sl("ah"),
+  Sl("ch"),
+  Sl("dh"),
+  Sl("bh")
+};
+global String reg_table_wide[] = {
+  Sl("ax"),
+  Sl("cx"),
+  Sl("dx"),
+  Sl("bx"),
+  Sl("sp"),
+  Sl("bp"),
+  Sl("si"),
+  Sl("di")
+};
+global String effective_address_calc_no_displacement[] = {
+  Sl("[bx + si]"),
+  Sl("[bx + di]"),
+  Sl("[bp + si]"),
+  Sl("[bp + di]"),
+  Sl("[si]"),
+  Sl("[di]"),
+  Sl("DIRECT ADDRESS"),
+  Sl("[bx]")
+};
+
+global String effective_address_calc_with_displacement[] = {
+  Sl("[bx + si + %u]"),
+  Sl("[bx + di + %u]"),
+  Sl("[bp + si + %u]"),
+  Sl("[bp + di + %u]"),
+  Sl("[si + %u]"),
+  Sl("[di + %u]"),
+  Sl("[bp + %u]"),
+  Sl("[bx + %u]")
+};
 
 global Instruction_Encoding instructions[] = {
   #include "instruction_encodings.inl"
@@ -76,7 +115,9 @@ global Instruction_Encoding instructions[] = {
 function inline u8
 get_bitfields(u16 data, u8 offset, u8 mask)
 {
-  return (data >> offset) & mask;
+  u8 shift  = data >> offset;
+  u8 masked = shift & mask; 
+  return masked;
 }
 
 /*
@@ -110,23 +151,25 @@ main()
     for (u32 i = 0; i < array_count(instructions); i += 1)
     {
       u8 opcode = low_byte & instructions[i].opcode_mask;
-      if (opcode == instructions[i].instruction)
+      u8 instruction_code = instructions[i].instruction & instructions[i].opcode_mask;
+
+      if (opcode == instruction_code)
       {
         instruction = instructions[i];
 
         // Fill data
-        instruction.data_low = low_byte;
-        if (instruction.data_size == 2) instruction.data_high = compiled_original_listing.cstring[byte_count++];
+        instruction.encoding_low = low_byte;
+        if (instruction.encoding_size == 2) instruction.encoding_high = compiled_original_listing.cstring[byte_count++];
 
-        if (instruction.S.offset != INVALID_OFFSET) instruction.S.data = get_bitfields(instruction.data, instruction.S.offset, instruction.S.mask);
-        if (instruction.W.offset != INVALID_OFFSET) instruction.W.data = get_bitfields(instruction.data, instruction.W.offset, instruction.W.mask);
-        if (instruction.D.offset != INVALID_OFFSET) instruction.D.data = get_bitfields(instruction.data, instruction.D.offset, instruction.D.mask);
-        if (instruction.V.offset != INVALID_OFFSET) instruction.V.data = get_bitfields(instruction.data, instruction.V.offset, instruction.V.mask);
-        if (instruction.Z.offset != INVALID_OFFSET) instruction.Z.data = get_bitfields(instruction.data, instruction.Z.offset, instruction.Z.mask);
+        if (instruction.S.offset != INVALID_OFFSET) instruction.S.data = get_bitfields(instruction.encoding, instruction.S.offset, instruction.S.mask);
+        if (instruction.W.offset != INVALID_OFFSET) instruction.W.data = get_bitfields(instruction.encoding, instruction.W.offset, instruction.W.mask);
+        if (instruction.D.offset != INVALID_OFFSET) instruction.D.data = get_bitfields(instruction.encoding, instruction.D.offset, instruction.D.mask);
+        if (instruction.V.offset != INVALID_OFFSET) instruction.V.data = get_bitfields(instruction.encoding, instruction.V.offset, instruction.V.mask);
+        if (instruction.Z.offset != INVALID_OFFSET) instruction.Z.data = get_bitfields(instruction.encoding, instruction.Z.offset, instruction.Z.mask);
 
-        if (instruction.MOD.offset != INVALID_OFFSET) instruction.MOD.data = get_bitfields(instruction.data, instruction.MOD.offset, instruction.MOD.mask);
-        if (instruction.REG.offset != INVALID_OFFSET) instruction.REG.data = get_bitfields(instruction.data, instruction.REG.offset, instruction.REG.mask);
-        if (instruction.R_M.offset != INVALID_OFFSET) instruction.R_M.data = get_bitfields(instruction.data, instruction.R_M.offset, instruction.R_M.mask);
+        if (instruction.MOD.offset != INVALID_OFFSET) instruction.MOD.data = get_bitfields(instruction.encoding, instruction.MOD.offset, instruction.MOD.mask);
+        if (instruction.REG.offset != INVALID_OFFSET) instruction.REG.data = get_bitfields(instruction.encoding, instruction.REG.offset, instruction.REG.mask);
+        if (instruction.R_M.offset != INVALID_OFFSET) instruction.R_M.data = get_bitfields(instruction.encoding, instruction.R_M.offset, instruction.R_M.mask);
 
         break;
       }
@@ -137,32 +180,63 @@ main()
       case DataTransfer_RegisterMemory_ToFrom_Register:
       {
         String* table = (instruction.W.data ? reg_table_wide : reg_table);
-
         switch (instruction.MOD.data)
         {
           case Mod_MemoryMode_NoDisplacement:
           {
-            printf("Mod_MemoryMode_NoDisplacement not implemented.\n");
-            goto end;
+            String destination = !instruction.D.data ? effective_address_calc_no_displacement[instruction.R_M.data] : table[instruction.REG.data];
+            String source      =  instruction.D.data ? effective_address_calc_no_displacement[instruction.R_M.data] : table[instruction.REG.data];
+            sprintf(output_buffer, "%s\nmov %s, %s", output_buffer, destination.cstring, source.cstring);
           }
           break;
           case Mod_MemoryMode_8BitDisplacement:
           {
-            printf("Mod_MemoryMode_8BitDisplacement not implemented.\n");
-            goto end;
+            String source;
+            String destination;
+
+            u8 temp[16]; // To put the instruction with displacement from format 
+            u8 displacement_low = compiled_original_listing.cstring[byte_count++];
+
+            if (instruction.D.data)
+            {
+              destination = table[instruction.REG.data];
+              source      = effective_address_calc_with_displacement[instruction.R_M.data];
+
+              sprintf(temp, source.cstring, displacement_low);
+              sprintf(output_buffer, "%s\nmov %s, %s", output_buffer, destination.cstring, temp);
+            }
+            else
+            {
+              destination = effective_address_calc_with_displacement[instruction.R_M.data];
+              source      = table[instruction.REG.data];
+              
+              sprintf(temp, destination.cstring, displacement_low);
+              sprintf(output_buffer, "%s\nmov %s, %s", output_buffer, temp, source.cstring);
+            }
+
           }
           break;
           case Mod_MemoryMode_16BitDisplacement:
           {
-            printf("Mod_MemoryMode_16BitDisplacement not implemented.\n");
-            goto end;
+            String destination = !instruction.D.data ? effective_address_calc_with_displacement[instruction.R_M.data] : table[instruction.REG.data];
+            String source      =  instruction.D.data ? effective_address_calc_with_displacement[instruction.R_M.data] : table[instruction.REG.data];
+
+            u8 displacement_low  = compiled_original_listing.cstring[byte_count++];
+            u8 displacement_high = compiled_original_listing.cstring[byte_count++];
+
+            u16 displacement = displacement_low;
+            displacement |= ((u16)displacement_high << 8);
+
+            u8 temp[16];
+            sprintf(temp, source.cstring, displacement);
+            sprintf(output_buffer, "%s\nmov %s, %s", output_buffer, destination.cstring, temp);
           }
           break;
           case Mod_RegisterMode_NoDisplacement:
           {
-            String destination =  instruction.D.data ? table[instruction.R_M.data] : table[instruction.REG.data];
-            String source      = !instruction.D.data ? table[instruction.R_M.data] : table[instruction.REG.data];
-            sprintf(output_buffer, "%s\nmov %s, %s", output_buffer, source.cstring, destination.cstring);
+            String destination = !instruction.D.data ? table[instruction.R_M.data] : table[instruction.REG.data];
+            String source      =  instruction.D.data ? table[instruction.R_M.data] : table[instruction.REG.data];
+            sprintf(output_buffer, "%s\nmov %s, %s", output_buffer, destination.cstring, source.cstring);
           }
           break;
         }
@@ -176,8 +250,17 @@ main()
       break;
       case DataTransfer_Immediate_To_Register:
       {
-        printf("DataTransfer_Immediate_To_Register not implemented.\n");
-        goto end;
+        String* table = (instruction.W.data ? reg_table_wide : reg_table);
+        u16 data = compiled_original_listing.cstring[byte_count++];
+
+        if (instruction.W.data)
+        {
+          u8 data_high = compiled_original_listing.cstring[byte_count++];
+          data |= ((u16)data_high << 8);
+        }
+
+        String destination = table[instruction.REG.data];
+        sprintf(output_buffer, "%s\nmov %s, %u", output_buffer, destination.cstring, data);
       }
       break;
       case DataTransfer_Memory_To_Accumulator:
@@ -208,9 +291,7 @@ main()
   }
 
 end:
-  printf("\n```%s\n```", output_buffer);
-
-#if 0
+  printf("\n\n```%s\n```\n\n", output_buffer);
 
   write_file(decompiled_asm_path, output_buffer, strlen(output_buffer));
 
@@ -220,54 +301,8 @@ end:
   system(command_buffer);
 
   u8 command_buffer_fc[256];
-  sprintf(command_buffer, "fc %s %s", decompiled_bin_path.cstring, original_bin_path.cstring);
-  printf("Copy paste to test:\n%s\n", command_buffer);
-#endif
+  sprintf(command_buffer_fc, "fc %s %s", decompiled_bin_path.cstring, original_bin_path.cstring);
+  system(command_buffer_fc);
 
   return 0;
 }
-
-/*
-Listing 039 bytes:
-0b10001001
-0b11011110
-0b10001000
-0b11000110
-0b10110001
-0b00001100
-0b10110101
-0b11110100
-0b10111001
-0b00001100
-0b00000000
-0b10111001
-0b11110100
-0b11111111
-0b10111010
-0b01101100
-0b00001111
-0b10111010
-0b10010100
-0b11110000
-0b10001010
-0b00000000
-0b10001011
-0b00011011
-0b10001011
-0b01010110
-0b00000000
-0b10001010
-0b01100000
-0b00000100
-0b10001010
-0b10000000
-0b10000111
-0b00010011
-0b10001001
-0b00001001
-0b10001000
-0b00001010
-0b10001000
-0b01101110
-0b00000000
-*/
